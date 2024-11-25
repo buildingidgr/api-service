2dab2ac3b790d5da4111692e1f55de06d45d0210
buildingidgr <78495205+buildingidgr@users.noreply.github.com>
2024-11-25 21:13:42 +0200

    v40

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,11 +1,6 @@
-import { PrismaClient } from '@prisma/client';
-import { config } from '../config';
+import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: config.databaseUrl,
-    },
-  },
-});
+const prisma = new PrismaClient()
+
+export { prisma }
 
